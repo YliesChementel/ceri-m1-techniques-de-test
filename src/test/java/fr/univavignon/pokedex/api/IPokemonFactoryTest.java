f8b3cbaae80012ca40649e96074619ad7c941be3
YliesChementel <ylies.chementel@alumni.univ-avignon.fr>
2024-10-18 17:09:22 +0200

    Update IPokemonFactoryTest.java with clean and working code

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,22 +1,43 @@
 package fr.univavignon.pokedex.api;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.plugins.MockMaker;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class IPokemonFactoryTest {
 
     @Mock
-    IPokemonFactory pokemonFactory = Mockito.mock(IPokemonFactory.class);
+    IPokemonFactory pokemonFactory;
 
     @Test
-    void shouldReturnInstanceOfPokemon() throws Exception {
-        //when(pokemonFactory.createPokemon(0,613,64,4000,4)).thenReturn();
+    void shouldCreatePokemonInstance() {
+        int index = 0;
+        String name = "Bulbizarre";
+        int attack = 126;
+        int defense = 126;
+        int stamina = 90;
+        int cp = 613;
+        int hp = 64;
+        int dust = 4000;
+        int candy = 4;
+        double iv = 54;
+
+        Pokemon expectedPokemon = new Pokemon(index, name, attack, defense, stamina, cp, hp, dust, candy, iv);
+        when(pokemonFactory.createPokemon(index, cp, hp, dust, candy)).thenReturn(expectedPokemon);
+
+        Pokemon result = pokemonFactory.createPokemon(index, cp, hp, dust, candy);
+
+        assertNotNull(result, "Le Pokémon créé ne doit pas être null");
+        assertEquals(expectedPokemon.getName(), result.getName(), "Le nom du Pokémon doit être celui attendu");
+        assertEquals(expectedPokemon.getCp(), result.getCp(), "Le CP du Pokémon doit être celui attendu");
+        assertEquals(expectedPokemon.getHp(), result.getHp(), "Le HP du Pokémon doit être celui attendu");
+        assertEquals(expectedPokemon.getDust(), result.getDust(), "La quantité de poussière du Pokémon doit être celle attendue");
+        assertEquals(expectedPokemon.getCandy(), result.getCandy(), "La quantité de bonbons du Pokémon doit être celle attendue");
+        assertEquals(expectedPokemon.getIv(), result.getIv(), "Le pourcentage de perfection IV du Pokémon doit être celui attendu");
     }
 }
