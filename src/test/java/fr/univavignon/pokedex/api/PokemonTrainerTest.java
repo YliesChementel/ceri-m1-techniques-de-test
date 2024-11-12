d517c174f3d0effde85cea1415d418eaeea99a3b
YliesChementel <ylies.chementel@alumni.univ-avignon.fr>
2024-11-12 17:34:28 +0100

    Add PokemonTrainerTest.java with 3 test : shouldReturnNameofTrainer, shouldReturnTeamofTrainer,shouldReturnPokedexofTrainer

---
+++ src/test/java/fr/univavignon/pokedex/api/PokemonTrainerTest.java
@@ -0,0 +1,38 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static fr.univavignon.pokedex.api.Team.INSTINCT;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+public class PokemonTrainerTest {
+
+    private PokemonTrainer pokemonTrainer;
+    private Pokedex pokedex;
+
+    @BeforeEach
+    public void setup() {
+        PokemonFactory pokemonFactory = new PokemonFactory();
+        IPokemonMetadataProvider metadataProvider = null;
+        pokedex = new Pokedex(metadataProvider,pokemonFactory);
+        pokemonTrainer = new PokemonTrainer("Sacha",INSTINCT,pokedex);
+    }
+
+    @Test
+    void shouldReturnNameofTrainer() throws Exception {
+        assertEquals(pokemonTrainer.getName(), "Sacha");
+    }
+
+    @Test
+    void shouldReturnTeamofTrainer() throws Exception {
+        assertEquals(pokemonTrainer.getTeam(), INSTINCT);
+    }
+
+    @Test
+    void shouldReturnPokedexofTrainer() throws Exception {
+        assertEquals(pokemonTrainer.getPokedex(), pokedex);
+    }
+}
