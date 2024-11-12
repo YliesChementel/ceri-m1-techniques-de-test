3a0f00117b925b42f54f57ad4d02a99ab3112cce
YliesChementel <ylies.chementel@alumni.univ-avignon.fr>
2024-11-12 15:41:32 +0100

    Update PokedexTest.java with 3 new test : shouldReturnListContainingAddedPokemon,shouldThrowExceptionWhenModifyingUnmodifiableListOfPokemons,shouldReturnListContainingAddedPokemonUsingCorrectOrder

--- src/test/java/fr/univavignon/pokedex/api/PokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokedexTest.java
@@ -3,6 +3,11 @@ package fr.univavignon.pokedex.api;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
@@ -35,6 +40,47 @@ public class PokedexTest {
         Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90,613,64,4000,4,56);
         pokedex.addPokemon(pokemon);
         assertEquals(pokemon, pokedex.getPokemon(0), "Le Pokémon récupéré doit être Bulbizarre");
+    }
+
+    @Test
+    void shouldReturnListContainingAddedPokemon() throws PokedexException {
+        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
+        pokedex.addPokemon(pokemon);
+
+        List<Pokemon> pokemons = pokedex.getPokemons();
+        assertEquals(1, pokemons.size(), "La liste des Pokémon devrait contenir 1 Pokémon");
+        assertEquals("Bulbizarre", pokemons.get(0).getName(), "Le Pokémon récupéré doit être Bulbizarre");
+    }
+
+    @Test
+    void shouldThrowExceptionWhenModifyingUnmodifiableListOfPokemons() throws PokedexException {
+        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
+        pokedex.addPokemon(pokemon);
+        List<Pokemon> pokemons = pokedex.getPokemons();
+
+        assertThrows(UnsupportedOperationException.class, () -> {
+            pokemons.add(new Pokemon(1, "Salamèche", 4, 4, 2, 39, 60, 500, 1, 15));
+        }, "La liste des Pokémon ne doit pas être modifiable");
+    }
+
+    @Test
+    void shouldReturnListContainingAddedPokemonUsingCorrectOrder() throws PokedexException {
+        Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
+        Pokemon pokemon1 = new Pokemon(1, "Herbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
+        Pokemon pokemon2 = new Pokemon(2, "Florizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
+
+        pokedex.addPokemon(pokemon);
+        pokedex.addPokemon(pokemon1);
+        pokedex.addPokemon(pokemon2);
+
+        Comparator<Pokemon> nameComparator = PokemonComparators.NAME;
+
+        List<Pokemon> pokemons = new ArrayList<>(pokedex.getPokemons());
+        pokemons.sort(nameComparator);
+
+        assertEquals("Bulbizarre", pokemons.get(0).getName(), "Le premier Pokémon devrait être Bulbizarre");
+        assertEquals("Florizarre", pokemons.get(1).getName(), "Le deuxième Pokémon devrait être Dracaufeu");
+        assertEquals("Herbizarre", pokemons.get(2).getName(), "Le troisième Pokémon devrait être Salamèche");
     }
 
 }
