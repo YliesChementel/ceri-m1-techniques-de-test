bebf5f8962b2908fb63b11eab403ed5fffdf1567
YliesChementel <ylies.chementel@alumni.univ-avignon.fr>
2024-12-11 10:48:10 +0100

    Add package-info.java and change to checkstyle.xml to add javadoc checks

---
+++ src/main/java/fr/univavignon/pokedex/api/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Ce package contient les interfaces, classes et énumérations pour gérer
+ * le système de Pokédex, y compris la création et la gestion des Pokémon,
+ * des métadonnées associées, des comparateurs, et des entraîneurs Pokémon.
+ *
+ * <p>Principaux éléments inclus :</p>
+ * <ul>
+ *   <li>Interfaces pour la gestion des pokémon, des métadonnées, et des entraîneurs.</li>
+ *   <li>Classes pour implémenter les entités comme les Pokémon, les entraîneurs, et le Pokédex.</li>
+ *   <li>Énumérations pour organiser les comparateurs de Pokémon et les équipes.</li>
+ *   <li>Gestion des exceptions spécifiques au Pokédex.</li>
+ * </ul>
+ */
+package fr.univavignon.pokedex.api;
